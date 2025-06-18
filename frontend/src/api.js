17dab0f4d516641c38f4cc8a15960c2bc4839fb7
pbrito <pbrito@kryptonbpo.com.br>
2025-06-18 09:22:33 -0300

    teste

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8007/api/v1',
+  baseURL: 'http://quickfix.kryptonbpo.com.br:8007/api/v1',
 });
 
 export default api;
